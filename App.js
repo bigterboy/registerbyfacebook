5e044ea7b3479a809d0feb317c04c46dc9448e68
Truong Phan Quí <>
2020-05-27 23:50:58 +0700

    add login by facebook

---
+++ App.js
@@ -0,0 +1,77 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React,{Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableHighlight,
+  Alert
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import {LoginButton, ShareDialog} from 'react-native-fbsdk';
+
+export default class App extends Component {
+  _shareLinkWithShareDialog = async () => {
+    const canShow = await ShareDialog.canShow(SHARE_LINK_CONTENT);
+    if (canShow) {
+      try {
+        const {isCancelled, postId} = await ShareDialog.show(
+          SHARE_LINK_CONTENT,
+        );
+        if (isCancelled) {
+          Alert.alert('Share cancelled');
+        } else {
+          Alert.alert('Share success with postId: ' + postId);
+        }
+      } catch (error) {
+        Alert.alert('Share fail with error: ' + error);
+      }
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <LoginButton
+          onLoginFinished={(error, data) => {
+            Alert.alert(JSON.stringify(error || data, null, 2));
+          }}
+        />
+        <TouchableHighlight onPress={this._shareLinkWithShareDialog}>
+          <Text style={styles.shareText}>Share link with ShareDialog</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  shareText: {
+    fontSize: 20,
+    margin: 10,
+  },
+});
